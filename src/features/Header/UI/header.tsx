091383024c2764dba54924d9cb03e30158ad275c
Asmat1k <dobrovti@mail.ru>
2023-12-15 14:36:55 +0300

    feat: change btns position on welcome page and add redirection when token expires

--- src/features/Header/UI/header.tsx
+++ src/features/Header/UI/header.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from '../../../app/appHooks';
 import { useLogout } from '../model/logout';
 import styles from './header.module.scss';
 import { useState } from 'react';
+import { LoginOutlined, LogoutOutlined, HomeOutlined, UserAddOutlined } from '@ant-design/icons';
 
 export function Header() {
   const logout = useLogout();
@@ -21,15 +22,30 @@ export function Header() {
 
   return (
     <header className={sticky ? styles.scrolled : styles.header}>
-      <Link to="/">Home</Link>
-      <Link to="/main">Main</Link>
-      {isAnonymous ? (
-        <Link to="/auth">Sign In</Link>
-      ) : (
-        <button className={styles.logout} onClick={logout}>
-          Sign Out
+      <div className={styles.item}>
+        <Link to="/">
+          <HomeOutlined />
+        </Link>
+      </div>
+      <div className={styles.item}>
+        <button className={styles.logout} onClick={() => console.log('click')}>
+          EN
         </button>
-      )}
+        {isAnonymous ? (
+          <div className={styles.links}>
+            <Link to="/auth">
+              <LoginOutlined />
+            </Link>
+            <Link to="/auth">
+              <UserAddOutlined />
+            </Link>
+          </div>
+        ) : (
+          <button className={styles.logout} onClick={logout}>
+            <LogoutOutlined />
+          </button>
+        )}
+      </div>
     </header>
   );
 }
--- src/widgets/welcome-block/UI/welcome-block.tsx
+++ src/widgets/welcome-block/UI/welcome-block.tsx
@@ -1,14 +1,30 @@
 import { Link } from 'react-router-dom';
 import styles from './welcome-block.module.scss';
+import { useAppSelector } from '../../../app/appHooks';
 
 export function WelcomeBlock() {
+  const { isAnonymous } = useAppSelector((state) => state.auth);
+
   return (
     <div className={styles.welcome}>
       <h2 className={styles.title}>GRAPHI-QL EDITOR</h2>
       <h4 className={styles.subtitle}>Go ahead and test it in our special editor</h4>
-      <Link to="/main" className={styles.btn}>
-        START
-      </Link>
+      {isAnonymous ? (
+        <div className={styles.links}>
+          <Link className={styles.btn} to="/auth">
+            Sign In
+          </Link>
+          <Link className={styles.btn} to="/auth">
+            Sign Up
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.links}>
+          <Link to="/main" className={styles.btn}>
+            Main page
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
