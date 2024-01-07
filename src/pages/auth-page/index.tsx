8b991a7657c3a3066a8b57926a6cc1469fd30a09
Iryna Kanavalchuk <irkinbk@gmail.com>
2024-01-07 15:27:07 +0300

    style: fix styles

--- src/pages/auth-page/index.tsx
+++ src/pages/auth-page/index.tsx
@@ -38,7 +38,6 @@ export default function AuthPage() {
           uid: currentUser.uid,
         }),
       );
-      navigate('/main');
     }
   });
 
--- src/widgets/footer/UI/footer.tsx
+++ src/widgets/footer/UI/footer.tsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
-import styles from './footer.module.scss';
 import { GithubOutlined } from '@ant-design/icons';
+import globalStyles from '../../../shared/common.module.scss';
+import styles from './footer.module.scss';
 
 export function Footer() {
   return (
     <footer className={styles.container}>
-      <Link to="https://github.com/codestudent24" className={styles.gitHub}>
-        <GithubOutlined />
-      </Link>
-      <div className={styles.year}>2023</div>
-      <Link to="https://rs.school/react/" className={styles.rss}>
-        <img className={styles.img} src="/rs_school_js.svg" alt="rss" />
-      </Link>
+      <div className={`${styles.footerWrapper} ${globalStyles.wrapper}`}>
+        <Link to="https://github.com/codestudent24" className={styles.gitHub}>
+          <GithubOutlined />
+        </Link>
+        <div className={styles.year}>2023</div>
+        <Link to="https://rs.school/react/" className={styles.rss}>
+          <img className={styles.img} src="/rs_school_js.svg" alt="rss" />
+        </Link>
+      </div>
     </footer>
   );
 }
