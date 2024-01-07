7fcfa8a05538d318028a31bf851974777ce75813
Iryna Kanavalchuk <irkinbk@gmail.com>
2024-01-07 03:03:43 +0300

    test: add Toolbar tests

---
+++ src/app/tests/Toolbar/index.test.tsx
@@ -0,0 +1,64 @@
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from '../../appStore';
+
+import Toolbar from '../../../features/graphiql/EditorContainer/Toolbar';
+
+describe('Toolbar', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Toolbar isEditable={false} setIsEditable={() => {}} />
+        </Provider>
+      </BrowserRouter>,
+    );
+
+    expect(getByText('Go')).toBeTruthy();
+    expect(getByText('Editor')).toBeTruthy();
+  });
+
+  it('fires the fetcher function on "Go" button click', () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error');
+    const consoleLogSpy = jest.spyOn(console, 'log');
+    consoleErrorSpy.mockImplementation(() => {});
+    consoleLogSpy.mockImplementation(() => {});
+    const fetcherMock = jest.fn();
+    const { getByText } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Toolbar isEditable={false} setIsEditable={() => {}} />
+        </Provider>
+      </BrowserRouter>,
+    );
+
+    fireEvent.click(getByText('Go'));
+    waitFor(() => {
+      expect(fetcherMock).toHaveBeenCalledTimes(1);
+    });
+    consoleErrorSpy.mockRestore();
+  });
+
+  it('toggles between "Editor" and "Results" on button click', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Toolbar isEditable={false} setIsEditable={() => {}} />
+        </Provider>
+      </BrowserRouter>,
+    );
+
+    const button = getByText('Editor');
+
+    fireEvent.click(button);
+    waitFor(() => {
+      expect(getByText('Results')).toBeTruthy();
+    });
+
+    fireEvent.click(button);
+    waitFor(() => {
+      expect(getByText('Editor')).toBeTruthy();
+    });
+  });
+});
