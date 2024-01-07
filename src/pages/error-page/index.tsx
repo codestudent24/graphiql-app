78cfa74c2365f5cd57e34e139b0e88a8abbab6fa
Iryna Kanavalchuk <irkinbk@gmail.com>
2024-01-07 16:37:06 +0300

    refactor: code refactor

--- src/pages/error-page/index.tsx
+++ src/pages/error-page/index.tsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <h3>Sorry, Something went wrong :)).</h3>
+      <h3>Sorry, Something went wrong :))</h3>
 
       <a className={styles.button} href="/">
         Return to the Main Page
